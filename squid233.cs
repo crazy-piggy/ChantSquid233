804d4c40a74e55f96463a8f1befe62d6ce0af5b7
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2020-11-22 04:44:25 -0600

    Create squid233.cs

---
+++ squid233.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace squid233
+{
+    class squid233
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("squid233");
+            }
+        }
+    }
+}
