de40d9604ad72b50f0e53b9fd149452771d8d72f
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2020-11-22 05:20:05 -0600

    asd

---
+++ squid233.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace squid233
+{
+    class squid233
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("squid233");
+            }
+        }
+    }
+}
